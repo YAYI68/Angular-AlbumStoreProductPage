ab068c33bfc300dd20db64d47b0e5f3f6c948ef6
YAYI68 <58443752+YAYI68@users.noreply.github.com>
2021-11-11 23:49:46 +0100

    init

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs';
 import { Album } from './album';
+import { Product } from './product';
 
 
 @Injectable({
@@ -10,10 +11,16 @@ import { Album } from './album';
 })
 export class ProductService {
   private _albumUrl:string = '../assets/album.json';
+  private _productsUrl:string = '../assets/products.json';
 
   constructor(private _http: Http) { }
+
   getAlbum(id:number):Observable<Album>{
    return this._http.get(this._albumUrl).map((response:Album) => response);
   }
+
+  getProducts():Observable<Product>{
+    return this._http.get(this._productsUrl).map((response:Product) => response);
+   }
  
 } 
