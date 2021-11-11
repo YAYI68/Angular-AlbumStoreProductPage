d65e47ce131d1efec9d64fc2504e296001b9d24b
YAYI68 <58443752+YAYI68@users.noreply.github.com>
2021-11-11 15:41:42 +0100

    init

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from 'app/album';
 import { ProductService } from '../product.service';
 
 @Component({
@@ -10,12 +11,9 @@ export class ProductDescriptionComponent implements OnInit {
 
   constructor(private _productService: ProductService ) { }
 
-    albumInfo;
+    albumInfo:Album;
   ngOnInit() {  
-    this._productService.getAlbum(1).subscribe({
-      next: response => this.albumInfo = response,
-  })
-    
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
   }
 
 }
